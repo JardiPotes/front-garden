85394078f6fcb5a4f07bc8003795dbd4f6ba0fc0
Vico4 <victoire.stahl@gmail.com>
2022-09-02 10:28:10 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import jardiLogo from './assets/jardi-logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+          <img src={jardiLogo} className="logo react" alt="jardi logo" />
+      </div>
+      <h1>Jardipotes !!!</h1>
+    </div>
+  )
+}
+
+export default App
