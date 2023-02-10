096a8efd5c76c4fffbce43a1f2b6ee5758306fee
Vico4 <victoire.stahl@gmail.com>
2023-02-10 16:49:59 +0100

    wip: pagination

--- src/pages/Gardens/index.tsx
+++ src/pages/Gardens/index.tsx
@@ -1,6 +1,7 @@
 import axios from "../../ClientProvider/axiosConfig";
 import { AxiosError, AxiosResponse } from "axios";
 import { useQuery } from "react-query";
+import { useState } from "react";
 
 type Garden = {
   address: string;
@@ -22,31 +23,43 @@ type QueryArgs = {
   error?: AxiosError | null;
   isLoading?: boolean;
   data?: void | GardenData | undefined;
+  isPreviousData: boolean;
 };
 
 type Result = AxiosResponse<string | unknown> & { data: GardenData };
 
 export const GardenPage: React.FC = () => {
-  const { error, isLoading, data }: QueryArgs = useQuery(
-    "gardens",
-    async () => {
+  const [offset, setOffset] = useState(0);
+  const { error, isLoading, data, isPreviousData }: QueryArgs = useQuery({
+    queryKey: ["gardens", offset],
+    queryFn: async () => {
       const data: GardenData | void = await axios
-        .get(`gardens`)
+        .get(`gardens?offset=${offset}`)
         .then((res: Result): GardenData => res.data)
         // eslint-disable-next-line no-console
         .catch((err: AxiosError) => console.log(err));
       return data;
-    }
-  );
+    },
+    keepPreviousData: true,
+  });
 
+  console.log(data);
   // TO-DO : UI & pagination
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   return (
-    <div>
-      {data?.results.map((result) => (
-        <div key={`garden_${result.id}`}>{result.title}</div>
-      ))}
-    </div>
+    <>
+      <div>
+        {data?.results.map((result) => (
+          <div key={`garden_${result.id}`}>{result.title}</div>
+        ))}
+      </div>
+      {data?.next && !isPreviousData && (
+        <button onClick={(): void => setOffset(offset + 2)}>next</button>
+      )}
+      {data?.previous && (
+        <button onClick={(): void => setOffset(offset - 2)}>previous</button>
+      )}
+    </>
   );
 };
