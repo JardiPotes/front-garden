afbb2ca4561c0be7b499ee999c5a5f1cce0b4fdf
Vico4 <victoire.stahl@gmail.com>
2023-06-09 12:16:33 +0200

    chore: homepage test

--- src/pages/HomePage/index.test.tsx
+++ src/pages/HomePage/index.test.tsx
@@ -2,11 +2,30 @@ import { fireEvent, render, screen } from "@testing-library/react";
 
 import HomePage from "./index";
 
+const mockedUsedNavigate = jest.fn();
+const mockedUseMutation = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate
+}));
+
+jest.mock("react-query", () => ({
+  ...jest.requireActual("react-query"),
+  useMutation: () => mockedUseMutation
+}));
+
 describe("render", () => {
   it("should render without crash", () => {
     render(<HomePage />);
     expect(
       screen.getByText("La Première Communauté pour Jardiner Ensemble !")
     ).toBeInTheDocument();
+  });
+  it("should open login modal when clicked", () => {
+    render(<HomePage />);
+    const login = screen.getByText("REJOINS-NOUS");
+    fireEvent.click(login);
+    expect(screen.getByText("ALORS, ON PLANTE ?")).toBeInTheDocument();
   });
 });
