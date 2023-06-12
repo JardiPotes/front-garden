e09baeebf7f3e9fa1318f017b5774a533b844afe
Vico4 <victoire.stahl@gmail.com>
2023-06-12 15:27:58 +0200

    chore: profile pages tests

--- src/pages/Messages/components/Preview/index.test.tsx
+++ src/pages/Messages/components/Preview/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { QueryClient } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import { MessagePreview } from "./index";
